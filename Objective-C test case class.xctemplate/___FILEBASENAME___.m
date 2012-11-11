cdb2767e6a436c56948c79bab85e168967f54d96
Rob Rix <rob.rix@me.com>
2012-11-11 10:28:20 -0500

    Attempts to fix a weird issue with the test case class template.

---
+++ Objective-C test case class.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,16 @@
+//  ___FILENAME___
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+-(void)test<#Case#> {
+	<##>
+}
+
+@end
